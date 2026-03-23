be32e7b1498aa8a6bc8a9757f33a0563c67031ee
dev-shreyash <bhosaleshreyash2@gmail.com>
2026-03-23 20:11:28 +0530

    setup

---
+++ server/evalur/src/main/java/com/evalur/EvalurApplication.java
@@ -0,0 +1,24 @@
+package com.evalur;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import io.github.cdimascio.dotenv.Dotenv;
+
+@SpringBootApplication
+public class EvalurApplication {
+
+	public static void main(String[] args) {
+		// Load .env variables into system properties
+        Dotenv dotenv = Dotenv.configure()
+                .ignoreIfMissing()
+                .load();
+        dotenv.entries().forEach(entry -> {
+            System.setProperty(entry.getKey(), entry.getValue());
+        });
+
+
+		SpringApplication.run(EvalurApplication.class, args);
+	}
+
+}
