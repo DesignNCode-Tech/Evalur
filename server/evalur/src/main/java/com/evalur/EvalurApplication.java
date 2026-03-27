f9c94d7f9c464502bdabfd188aba63aa542f3643
dev-shreyash <bhosaleshreyash2@gmail.com>
2026-03-27 18:28:08 +0530

    feat: implement global backend foundation (#6)

--- server/evalur/src/main/java/com/evalur/EvalurApplication.java
+++ server/evalur/src/main/java/com/evalur/EvalurApplication.java
@@ -2,10 +2,12 @@ package com.evalur;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class EvalurApplication {
 
 	public static void main(String[] args) {
