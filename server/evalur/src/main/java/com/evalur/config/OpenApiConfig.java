ee60917b67481aa0282db37ec654b1caf9e0d431
dev-shreyash <bhosaleshreyash2@gmail.com>
2026-04-14 17:54:02 +0530

    Feat: implemented swagger api docs

---
+++ server/evalur/src/main/java/com/evalur/config/OpenApiConfig.java
@@ -0,0 +1,31 @@
+package com.evalur.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Evalur API")
+                        .version("1.0")
+                        .description("B2B AI Assessment Platform Documentation"))
+                // This section adds the "Authorize" lock icon to the UI
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
+                .components(new Components()
+                        .addSecuritySchemes("bearerAuth",
+                                new SecurityScheme()
+                                        .name("bearerAuth")
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")));
+    }
+}
