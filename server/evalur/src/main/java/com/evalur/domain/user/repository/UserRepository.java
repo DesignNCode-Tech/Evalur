8a40a1d3be1195d4db3b338b3075053900982a94
dev-shreyash <bhosaleshreyash2@gmail.com>
2026-04-14 18:33:26 +0530

    feat: added issue #32 requests

--- server/evalur/src/main/java/com/evalur/domain/user/repository/UserRepository.java
+++ server/evalur/src/main/java/com/evalur/domain/user/repository/UserRepository.java
@@ -1,6 +1,7 @@
 package com.evalur.domain.user.repository;
 
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -10,4 +11,6 @@ import com.evalur.domain.user.entity.User;
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
     boolean existsByEmail(String email);
+
+    List<User> findByOrganizationId(Long organizationId);
 }
